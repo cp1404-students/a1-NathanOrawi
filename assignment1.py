297ce79f0ebbf4602de818d69cc90f3e96241fe0
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-10-25 14:57:31 +1000

    Add menu()

--- assignment1.py
+++ assignment1.py
@@ -4,10 +4,37 @@ Date started: 25/10/2023
 GitHub URL: https://github.com/cp1404-students/a1-NathanOrawi.git
 """
 
+MENU = """Menu:
+D - Display songs
+A - Add new song
+C - Complete a song
+Q - Quit"""
+
 
 def main():
     """..."""
-    print("Song List 1.0 - by Your Name")
+    print("Song List 1.0 - by Nathan Orawi")
+    menu()
+
+
+def menu():
+    """display a menu for the user to choose from"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != 'Q':
+        if choice == 'D':
+            print("Songs 1")
+            print("Songs 2")
+            print("Songs 3")
+        elif choice == 'A':
+            print("Song added")
+        elif choice == 'C':
+            print("Song complete")
+        else:
+            print("Invalid menu choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("X songs saved to songs.csv")
 
 
 if __name__ == '__main__':
