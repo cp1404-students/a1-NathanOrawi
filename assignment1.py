5966f7f89e09ba61da39a29b326df79f5cee01a6
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-10-26 10:07:42 +1000

    Add get_data() and format_data(data)

--- assignment1.py
+++ assignment1.py
@@ -4,6 +4,7 @@ Date started: 25/10/2023
 GitHub URL: https://github.com/cp1404-students/a1-NathanOrawi.git
 """
 FILE_NAME = "songs.csv"
+SONGS = []
 
 MENU = """Menu:
 D - Display songs
@@ -25,9 +26,8 @@ def menu():
     choice = input(">>> ").upper()
     while choice != 'Q':
         if choice == 'D':
-            print("Songs 1")
-            print("Songs 2")
-            print("Songs 3")
+            data = get_data()
+            format_data(data)
         elif choice == 'A':
             print("Song added")
         elif choice == 'C':
@@ -40,11 +40,50 @@ def menu():
 
 
 def read_from_file(file):
-    """reads from a file"""
+    """reads from a file and display string line by line"""
     in_file = open(file)
-    song = in_file.read()
-    print(song)
+    songs = in_file.read()
+    print(songs)
     in_file.close()
+    return songs
+
+
+def create_nested_list_from_file(file):
+    in_file = open(file)
+    songs = []
+    for line in in_file:
+        song = (line.strip().split('  '))
+        # print(song)
+        songs.append(song)
+    # print(songs)
+    in_file.close()
+    return songs
+
+
+def format_data(data):
+    """Format and Display subject details with supporting data """
+    for parts in data:
+        for part in parts:
+            pass
+        print(f"{parts[3]:3} {parts[0]:30} - {parts[1]:20} ({parts[2]})")
+
+
+def get_data():
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
+    input_file = open(FILE_NAME)
+    for line in input_file:
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        # print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        # print(parts)  # See if that worked
+        data.append(parts)
+        # print("----------")
+    input_file.close()
+    return data
 
 
 if __name__ == '__main__':
