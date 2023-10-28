8bade81bf4c15fbfde5f8f9dea9d59934b0ff468
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-10-28 21:47:19 +1000

    Add comments and quit_song_list()

--- assignment1.py
+++ assignment1.py
@@ -3,6 +3,8 @@ Name: Nathan Orawi
 Date started: 25/10/2023
 GitHub URL: https://github.com/cp1404-students/a1-NathanOrawi.git
 """
+from operator import itemgetter
+
 FILE_NAME = "songs.csv"
 SONGS = []  # List of dictionaries
 SORTED_SONGS = []  # Sorted SONGS by value 'year'
@@ -17,16 +19,22 @@ Q - Quit"""
 
 
 def main():
-    """..."""
+    """main adds the fields to 'song.csv' then makes
+    a list of dictionaries of field value pairs and
+    is manipulated according to the user in the menu """
+
+    # Prepares csv file for processing
     add_fields_to_csv()
     make_dictionary()
+
+    # Display of program structure is set here
     read_csv_file()
     print("Song List 1.0 - by Nathan Orawi")
-    menu()
+    menu()  # Where 'song.csv' is manipulated
 
 
 def menu():
-    """display a menu for the user to choose from"""
+    """display a menu of actions for the user to choose from"""
     print(f"{len(SONGS)} songs loaded")
     print(MENU)
     choice = input(">>> ").upper()
@@ -42,48 +50,14 @@ def menu():
         print(MENU)
         choice = input(">>> ").upper()
     print(f"{len(SORTED_SONGS) + len(SONGS)} songs saved to {FILE_NAME} \nMake some music!")
-
-
-def add_fields_to_csv():
-    """adds field to the first line of the csv file"""
-    with open("songs.csv") as file:
-        first_line = file.readline()
-        if first_line != 'title,artist,year,learned status\n':
-            csv_field = 'title,artist,year,learned status\n'
-            in_file = open("songs.csv")
-            line = in_file.read()
-            fielded_file = csv_field + line
-            in_file.close()
-
-            out_file = open("songs.csv", 'w')
-            print(fielded_file.rstrip('\n'), file=out_file)
-            out_file.close()
-
-
-def read_csv_file():
-    """reads from a file and display csv song line by line"""
-    in_file = open(FILE_NAME)
-    songs_csv = in_file.read()
-    in_file.close()
-    print(songs_csv)
-
-
-def make_dictionary():
-    """makes a song dictionary from csv file"""
-    with open(FILE_NAME, 'r') as csv_file:
-        songs_csv = csv_file.readlines()
-        song_fields = [fields.strip() for fields in songs_csv[0].split(',')]
-        for song_csv in songs_csv[1:]:
-            song_values = [value.strip() for value in song_csv.split(',')]
-            song_field_to_song_value = {field: value for field, value in zip(song_fields, song_values)}
-            SONGS.append(song_field_to_song_value)
+    quit_song_list()
 
 
 def display_song():
     """Takes the song dictionary then formats and Displays it """
     song_number = 1
     number_of_learned_song = 0
-    SORTED_SONGS = sorted(SONGS, key=lambda x: x['year'])  # list of song dictionary
+    SORTED_SONGS = sorted(SONGS, key=itemgetter('year'))  # list of song dictionary
     print(SORTED_SONGS)
     for sorted_song_field_to_song_value in SORTED_SONGS:
         title, artist, year, learned_status = sorted_song_field_to_song_value.values()
@@ -129,7 +103,7 @@ def complete_song():
     """marks a song chosen by its number as learned"""
     learned_index = 0
     print("Enter the number of a song to mark as learned.")
-    sorted_song_fields_to_song_values = sorted(SONGS, key=lambda x: x['year'])
+    sorted_song_fields_to_song_values = sorted(SONGS, key=itemgetter('year'))
     is_valid_input = False
     while not is_valid_input:
         mark = int(input(">>> "))
@@ -149,6 +123,56 @@ def complete_song():
                     break
             sorted_song_fields_to_song_values[learned_index]['learned status'] = 'l'
             is_valid_input = True
+
+
+def quit_song_list():
+    """quits the program and save the update song list to 'song.csv'"""
+    out_file = open("songs.csv", 'w')
+    for song in SONGS:
+        # My favourite method is pop(). I had to find a way to incorporate it into the code :)
+        title = (list(song.values())).pop(0)
+        artist = (list(song.values())).pop(1)
+        year = (list(song.values())).pop(2)
+        learned_status = (list(song.values())).pop(3)
+        songs_csv = ("{},{},{},{}".format(title, artist, year, learned_status))
+
+        print(songs_csv, file=out_file)
+    out_file.close()
+
+
+def add_fields_to_csv():
+    """adds field to the first line of the csv file"""
+    with open("songs.csv") as file:
+        first_line = file.readline()
+        if first_line != 'title,artist,year,learned status\n':
+            csv_field = 'title,artist,year,learned status\n'
+            in_file = open("songs.csv")
+            line = in_file.read()
+            fielded_file = csv_field + line
+            in_file.close()
+
+            out_file = open("songs.csv", 'w')
+            print(fielded_file.rstrip('\n'), file=out_file)
+            out_file.close()
+
+
+def read_csv_file():
+    """reads from a file and display csv song line by line"""
+    in_file = open(FILE_NAME)
+    songs_csv = in_file.read()
+    in_file.close()
+    print(songs_csv)
+
+
+def make_dictionary():
+    """makes a song dictionary from csv file"""
+    with open(FILE_NAME, 'r') as csv_file:
+        songs_csv = csv_file.readlines()
+        song_fields = [fields.strip() for fields in songs_csv[0].split(',')]
+        for song_csv in songs_csv[1:]:
+            song_values = [value.strip() for value in song_csv.split(',')]
+            song_field_to_song_value = {field: value for field, value in zip(song_fields, song_values)}
+            SONGS.append(song_field_to_song_value)
 
 
 if __name__ == '__main__':
