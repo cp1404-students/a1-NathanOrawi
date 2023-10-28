a88fe9ba2b890458702d3e29bae49619f0dc6389
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-10-28 17:29:46 +1000

    Refactor Global variables

--- assignment1.py
+++ assignment1.py
@@ -4,8 +4,10 @@ Date started: 25/10/2023
 GitHub URL: https://github.com/cp1404-students/a1-NathanOrawi.git
 """
 FILE_NAME = "songs.csv"
-songs = []
-sorted_songs = []
+SONGS = []
+SORTED_SONGS = []
+YEAR_OF_FIRST_RECORDED_SONG = 1860
+CURRENT_YEAR = 2023
 
 MENU = """Menu:
 D - Display songs
@@ -30,7 +32,7 @@ def main():
 
 def menu():
     """display a menu for the user to choose from"""
-    print(f"{len(songs)} songs loaded")
+    print(f"{len(SONGS)} songs loaded")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != 'Q':
@@ -44,7 +46,7 @@ def menu():
             print("Invalid menu choice")
         print(MENU)
         choice = input(">>> ").upper()
-    print(f"{len(sorted_songs) + len(songs)} songs saved to {FILE_NAME} \nMake some music!")
+    print(f"{len(SORTED_SONGS) + len(SONGS)} songs saved to {FILE_NAME} \nMake some music!")
 
 
 def read_from_file():
@@ -63,17 +65,17 @@ def make_dictionary():
         for line in songs_csv[1:]:
             values = [value.strip() for value in line.split(',')]
             song = {field: value for field, value in zip(fields, values)}
-            songs.append(song)
+            SONGS.append(song)
 
 
 def format_data():
     """Takes the song dictionary then formats and Displays it """
     i = 1
     learned = 0
-    sorted_songs = sorted(songs, key=lambda x: x['year'])
-    # print(sorted_songs)
+    SORTED_SONGS = sorted(SONGS, key=lambda x: x['year'])
+    # print(SORTED_SONGS)
     # Print the sorted list
-    for sorted_song in sorted_songs:
+    for sorted_song in SORTED_SONGS:
         title, artist, year, learned_status = sorted_song.values()
         if learned_status == 'u':
             learned_status = '*'
@@ -82,7 +84,7 @@ def format_data():
             learned_status = ''
         print(f"{i:>2}. {learned_status:2} {title:30} - {artist:25} ({year:>04})")
         i += 1
-    print(f"{len(sorted_songs) - learned} songs learned, {learned} songs still to learn.")
+    print(f"{len(SORTED_SONGS) - learned} songs learned, {learned} songs still to learn.")
 
 
 def mark_as_learned():
@@ -94,17 +96,17 @@ def mark_as_learned():
     #     print("Number must be > 0.")
     #     mark = int(input(">>> "))
 
-    sorted_songs = sorted(songs, key=lambda x: x['year'])
+    SORTED_SONGS = sorted(SONGS, key=lambda x: x['year'])
     is_valid_input = False
     while not is_valid_input:
         mark = int(input(">>> "))
         if mark <= 0:
             print("Number must be > 0.")
-        elif mark > len(sorted_songs):
+        elif mark > len(SORTED_SONGS):
             print("Invalid song number")
         else:
-            # print(enumerate(sorted_songs))
-            for index, item in enumerate(sorted_songs):
+            # print(enumerate(SORTED_SONGS))
+            for index, item in enumerate(SORTED_SONGS):
                 # print(f" {index} {item}")
                 if index == (mark - 1):
                     title, artist, year, learned_status = item.values()
@@ -114,10 +116,10 @@ def mark_as_learned():
                         print(f"{title} by {artist} learned")
                         learned_index = index
                     break
-            sorted_songs[learned_index]['learned status'] = 'l'
+            SORTED_SONGS[learned_index]['learned status'] = 'l'
             is_valid_input = True
 
-    # print(sorted_songs)
+    # print(SORTED_SONGS)
 
 
 def add_song():
@@ -136,14 +138,14 @@ def add_song():
             new_year = int(input("Year: "))
             if new_year <= 0:
                 print("Number must be > 0.")
-            elif new_year < 1860 or new_year >= 2024:
+            elif new_year < YEAR_OF_FIRST_RECORDED_SONG or new_year >= CURRENT_YEAR:
                 print(f"Song record do not exist in the year {new_year}")
             else:
                 is_valid_input = True
         except ValueError:
             print("Invalid input; enter a valid number.")
 
-    songs.append({'title': new_title, 'artist': new_artist, 'year': str(new_year), 'learned status': 'u'})
+    SONGS.append({'title': new_title, 'artist': new_artist, 'year': str(new_year), 'learned status': 'u'})
     print(f"{new_title} by {new_artist} ({new_year}) added to song list.")
 
 
